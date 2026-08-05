265e69ce2a4a3b30bd86b5d9f9d20561847a0bc3
Samar Anand <samaranand9798@gmail.com>
2026-08-05 19:27:49 +0530

    Time: 0 ms (100%), Space: 9 MB (55.13%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -3,19 +3,18 @@ public:
     string removeOuterParentheses(string s) {
         string result="";
         int level=0;
-
         for(char ch:s){
             if(ch=='('){
-                    if(level>0) result+=ch;
-                    level++;
-                
+                 if(level>0) result+=ch;
+               
+                level++;
+               
             }
             else if(ch==')'){
                 level--;
-                if(level>0)result+=ch;
+                if(level>0) result+=ch;
             }
         }
         return result;
-        
     }
 };
