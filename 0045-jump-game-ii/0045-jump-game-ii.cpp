5a6a095214325e94d7670f6b47a470b1f21f2603
Samar Anand <samaranand9798@gmail.com>
2026-08-08 23:16:22 +0530

    Time: 0 ms (100%), Space: 20.6 MB (79.21%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int farthest=0,currend=0,jumps=0;
+        for(int i=0;i<nums.size()-1;i++){
+            farthest=max(farthest,i+nums[i]);
+            if(i==currend){
+                jumps++;
+                currend=farthest;
+            }
+
+        }
+        return jumps;
+    }
+};
