b01d143a85ccd302fb631cc9050e3e7304f7cf58
Samar Anand <samaranand9798@gmail.com>
2026-09-04 22:33:05 +0530

    Time: 3 ms (91.52%), Space: 13 MB (74.14%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    
+    int editdistance(string& word1, string& word2, 
+                     int i, int j, vector<vector<int>>& dp) {
+        
+        
+        if(i < 0) {
+            return j + 1;
+        }
+
+       
+        if(j < 0) {
+            return i + 1;
+        }
+
+     
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        
+        if(word1[i] == word2[j]) {
+            return dp[i][j] = editdistance(
+                word1, word2, i - 1, j - 1, dp
+            );
+        }
+
+        
+        return dp[i][j] = 1 + min(
+            editdistance(word1, word2, i - 1, j - 1, dp), 
+            min(
+                editdistance(word1, word2, i - 1, j, dp),
+                editdistance(word1, word2, i, j - 1, dp)
+            )
+        );
+    }
+
+public:
+    int minDistance(string word1, string word2) {
+        
+        int n = word1.size();
+        int m = word2.size();
+
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+
+        return editdistance(word1, word2, n - 1, m - 1, dp);
+    }
+};
