5194bab698aa15a4ceb185b27c6ab78890aedb79
Samar Anand <samaranand9798@gmail.com>
2026-09-06 21:41:30 +0530

    Time: 15 ms (6.14%), Space: 24.9 MB (11.35%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,11 +3,14 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
         vector<vector<int>>merged;
-        for(auto interval:intervals){
+        for(auto interval: intervals){
             if(merged.empty() || merged.back()[1]<interval[0]){
                 merged.push_back(interval);
             }
-            merged.back()[1]=max(merged.back()[1],interval[1]);
+            else{
+                merged.back()[1]=max(merged.back()[1],interval[1]);
+            }
+
         }
         return merged;
     }
