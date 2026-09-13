861b594d89919f4543d1786e9b30dec1e63fbcf3
Samar Anand <samaranand9798@gmail.com>
2026-09-14 00:27:34 +0530

    Time: 2 ms (30.65%), Space: 16.6 MB (97.49%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,68 +1,44 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-
-        int n = grid.size();
-        int m = grid[0].size();
-
-        queue<pair<pair<int,int>,int>> q;
-
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-
-        int fresh = 0;
-
-       
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 2) {
-                    q.push({{i, j}, 0});
-                    vis[i][j] = 2;
+        if(grid.empty()) return 0;
+        int m=grid.size();
+        int n=grid[0].size();
+        int days=0;
+        int tot=0;
+        int cnt=0;
+        queue<pair<int,int>>rotten;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]!=0){
+                    tot++;
                 }
-                else {
-                    vis[i][j] = 0;
-                }
-
-                if(grid[i][j] == 1)
-                    fresh++;
+                if(grid[i][j]==2)
+                rotten.push({i,j});
             }
         }
-
-        int tm = 0;
-
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
-
-        while(!q.empty()) {
-
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-
-            q.pop();
-
-            tm = max(tm, t);
-
-            for(int i = 0; i < 4; i++) {
-
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-
-                if(nrow >= 0 && nrow < n &&
-                   ncol >= 0 && ncol < m &&
-                   vis[nrow][ncol] != 2 &&
-                   grid[nrow][ncol] == 1) {
-
-                    vis[nrow][ncol] = 2;
-                    fresh--;
-
-                    q.push({{nrow, ncol}, t + 1});
+        int dx[4]={0,0,1,-1};
+        int dy[4]={1,-1,0,0};
+        while(!rotten.empty()){
+            int k= rotten.size();
+            cnt+=k;
+            while(k--){
+                int x=rotten.front().first;
+                int y=rotten.front().second;
+                rotten.pop();
+                for(int i=0;i<4;i++){
+                    int nx=x+dx[i];
+                    int ny=y+dy[i];
+                    if(nx<0||ny<0||nx>=m||ny>=n||grid[nx][ny]!=1)
+                    continue;
+                    grid[nx][ny]=2;
+                    rotten.push({nx,ny});
                 }
             }
+            if(!rotten.empty())
+            days++;
+
         }
-
-        if(fresh != 0)
-            return -1;
-
-        return tm;
+        return tot==cnt?days:-1;
     }
 };
