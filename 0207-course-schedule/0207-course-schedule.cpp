215d147e179fea2d68552f3253d5d9f028c6204d
Samar Anand <samaranand9798@gmail.com>
2026-09-26 01:21:20 +0530

    Time: 4 ms (65.15%), Space: 20.1 MB (25.62%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,43 +1,36 @@
 class Solution {
-private:
-    bool dfs(int node, vector<int>& vis, vector<int>& pathVis, vector<int> adj[]) {
-        vis[node] = 1;
-        pathVis[node] = 1;
-
-        for (auto adjacentNode : adj[node]) {
-            if (!vis[adjacentNode]) {
-                if (dfs(adjacentNode, vis, pathVis, adj))
-                    return true;
-            }
-            else if (pathVis[adjacentNode]) {
-                return true;
+    public:
+    bool dfs(int course, vector<vector<int>>& graph,vector<int>& state){
+        if(state[course]==1){
+            return false;
+        }
+        if(state[course]==2){
+            return true;
+        }
+        state[course]=1;
+        for(int next: graph[course]){
+            if(!dfs(next,graph,state)){
+                return false;
             }
         }
-
-        pathVis[node] = 0;
-        return false;
+        state[course]=2;
+        return true;
     }
-
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-
-        vector<int> adj[numCourses];
-
-        // Build adjacency list
-        for (auto it : prerequisites) {
-            adj[it[1]].push_back(it[0]);
+        vector<vector<int>>graph(numCourses);
+        for(auto prerequisite: prerequisites){
+            int course=prerequisite[0];
+            int prerequisitecourse=prerequisite[1];
+            graph[prerequisitecourse].push_back(course);
         }
-
-        vector<int> vis(numCourses, 0);
-        vector<int> pathVis(numCourses, 0);
-
-        for (int i = 0; i < numCourses; i++) {
-            if (!vis[i]) {
-                if (dfs(i, vis, pathVis, adj))
-                    return false;   
+        vector<int>state(numCourses,0);
+        for(int course=0;course<numCourses;course++){
+            if(!dfs(course,graph,state)){
+                return false;
             }
         }
-
-        return true;    
+        return true;
+        
     }
 };
